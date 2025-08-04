8d72f87747b9136c70fb0d96102433d2fe5a94f1
Tyler Johnson <tyler.jpeg@gmail.com>
2025-08-04 13:03:58 -0400

    Fix layout.tsx to prevent middleware invocation error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,20 @@
 // app/layout.tsx
-
 import '../globals.css'
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
+import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Grim Lister',
-  description: 'An eBay listing assistant built by Grim Vessel.',
-};
+  description: 'The ultimate vintage listing assistant.',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
-  );
+  )
 }
